5a028ea31b21d57de2187fd3b6da6471254c737b
HAMISH LYALL <101614208@student.swin.edu.au>
2018-06-17 16:55:54 +1000

    Add project files.

---
+++ Dip_SWD_SubTask9_Async_ClassLibrary/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Dip_SWD_SubTask9_Async_ClassLibrary
+{
+    public class Calculator
+    {
+        public long CalculateNthPrimeNumber()
+        {
+            int count = 0;
+            long a = 2;
+            while (count < 40000)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+
+    }
+}
---
+++ Dip_SWD_SubTask9_Async_ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dip_SWD_SubTask9_Async_ClassLibrary;
+
+namespace Dip_SWD_SubTask9_Async_ConsoleApp
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            CalculateNthPrimeNumber();
+            
+            Console.ReadKey();
+        }
+
+        private static long CalculateNthPrimeNumber()
+        {
+            int count = 0;
+            long a = 2;
+            while (count < 40000)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+    }
+}
